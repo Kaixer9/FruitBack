093046544ffd748273bde913180d45eab45b0276
Arcadio <138514290+Kaixer9@users.noreply.github.com>
2023-11-27 11:20:34 +0000

    Delete Fruit directory

--- Fruit/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { checkAuth } = require("../middlewares/auth");
-
-router.use('/auth', require('./auth.router'))
-
-router.use('/user', require('./user'))
-router.use('/frutas', require('./frutas'))
-
-module.exports = router
